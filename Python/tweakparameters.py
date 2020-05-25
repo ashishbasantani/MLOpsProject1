644aad1066d80a5830664ee8ee8dccd8b004b6f2
ashishbasantani <ashishbasantani77@gmail.com>
2020-05-25 19:52:47 +0530

    Add files via upload

--- Python/tweakparameters.py
+++ Python/tweakparameters.py
@@ -1,1 +1,21 @@
-print('File for tweaking parameters')
+import pandas as pd
+
+parameters = pd.read_csv('hyperparameters.csv')
+
+epoch = (parameters['epochs'])
+numberoflayers = (parameters['layers'])
+numberofneurons = (parameters['neurons'])
+activationfunction = (parameters['activationfunction'])
+learningrate = (parameters['learningrate'])
+
+epoch = int(epoch[0])
+numberoflayers = int(numberoflayers[0])
+numberofneurons = int(numberofneurons[0])
+actiavationfunction = str(activationfunction[0])
+learningrate = float(learningrate[0])
+
+epoch = epoch + 5
+numberoflayers = numberoflayers + 2
+numberofneurons = numberofneurons + 3
+
+parameters.to_csv('hyperparameters.csv')
